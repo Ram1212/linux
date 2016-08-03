7f1d029688243f1e4e015a6f0f09d4f4adc39f27
Ramesh <linux4121990@gmail.com>
2016-08-03 22:36:41 +0000

    This is first bash program

---
+++ helloworld.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#uthor:Ramesh
+# Prpose:Hello world
+
+#Variables here
+MSG1="Hi hello guys welcome to Linux world"
+MSG2="This is Ramesh"
+
+#begin code
+clear
+echo to day is `date`
+echo $MSG1
+echo $MSG2
+echo " what is your name?"
+read name
+clear
+echo "Hi $name how are you"
+read feeling
+echo yah feeling $feeling
+#End
+
